bd909900749dd1151fa1cd3cb71d6ead6c2dd1fa
SAIM878 <146725755+SAIM878@users.noreply.github.com>
2024-06-19 11:09:10 +0000

    vaiables working

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const account_id = 12345
+let   account_email = "ali@gmail.com"
+var   account_password = "12345"
+let account_state;
+account_city = "haripur"
+console.log(account_id);
+account_email = "ali123@gmail.com"
+account_password = "123"
+account_city = "KArachi"
+console.table([account_email,account_id,account_password,account_city,account_state])
