0dc83148c92a7a17d0cd34fcac5f640e82e3624b
SAIM878 <146725755+SAIM878@users.noreply.github.com>
2024-06-20 18:06:09 +0000

    conversion of data types

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+// let score = '33ab'
+// let score = null
+let score = undefined
+// console.log(typeof score)
+let valueinnumber = Number(score)
+// console.log(typeof valueinnumber)
+// console.group(valueinnumber)
+
+let islogedin = 1
+let boleanislogedin = Boolean(islogedin)
+console.log(typeof boleanislogedin)
+console.log(boleanislogedin)
+// 1 = true; 0= false
+// "" = false; "saim" = true
